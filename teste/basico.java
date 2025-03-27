f91f57d36c53f630b6c8bcf846fb24fb7423e0c1
Pedro <pedrosilvanovais7@gmail.com>
2025-03-27 16:31:24 -0300

    Removi conteudo de tipos primitivos

--- teste/basico.java
+++ teste/basico.java
@@ -2,18 +2,7 @@
 public class basico {
 
     public static void main(String[] args){
-        System.out.println("Hello, World !");
-        int idade = 3;
         
-        //float altura = (float) 1.78;
-        float altura = 1.78f;
-
-        char letra = 'P'; // Só aceita 1 letra
-        boolean casado = true;
-
-        String palavra = "casa";
-        System.out.println(palavra);
-
         String primeiroNome = "Pedro";
         String ultimoNome = "Silva novais";
         String nomeCompleto = nomeCompleto(primeiroNome, ultimoNome);
