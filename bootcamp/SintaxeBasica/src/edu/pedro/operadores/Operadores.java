d7c714d6848443f13e822859d5183866e5d97342
PedroSNovais <pedrosilvanovais@gmail.com>
2025-03-27 18:55:01 -0300

    usando condição

---
+++ bootcamp/SintaxeBasica/src/edu/pedro/operadores/Operadores.java
@@ -0,0 +1,17 @@
+package edu.pedro.operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // Operadores são a mesma coisa do python
+        String resultado = "";
+        int a = 5;
+        int b = 5;
+        if (a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "Falso";
+        
+
+        System.out.println(resultado);
+    }
+}
