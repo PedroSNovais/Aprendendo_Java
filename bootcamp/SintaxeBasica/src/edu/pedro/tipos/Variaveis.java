5211c4e350682ba533bfecb5cb1c50b466ed35d8
PedroSNovais <pedrosilvanovais@gmail.com>
2025-03-27 18:31:09 -0300

    variaveis e constantes

--- bootcamp/SintaxeBasica/src/edu/pedro/tipos/Variaveis.java
+++ bootcamp/SintaxeBasica/src/edu/pedro/tipos/Variaveis.java
@@ -3,5 +3,15 @@ package edu.pedro.tipos;
 public class Variaveis {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        /* variaveis podem mudar
+        int idade = 19;
+        double salario = 2500.33;
+        float atura = 1.78f; */
+
+        // constantes não mudam e tem CAIXA ALTA
+        final double VALOR_DE_PI = 3.14;
+        // A palavra 'final' define as constantes
+        System.out.println(VALOR_DE_PI);
+
     }
 }
