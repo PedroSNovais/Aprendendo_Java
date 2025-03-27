aaf462f46e4091352a092a5e691d05e10e9d8b0e
PedroSNovais <pedrosilvanovais@gmail.com>
2025-03-27 19:05:17 -0300

    Simplificação do IF ELSE

--- bootcamp/SintaxeBasica/src/edu/pedro/operadores/Operadores.java
+++ bootcamp/SintaxeBasica/src/edu/pedro/operadores/Operadores.java
@@ -3,14 +3,17 @@ package edu.pedro.operadores;
 public class Operadores {
     public static void main(String[] args) {
         // Operadores são a mesma coisa do python
-        String resultado = "";
-        int a = 5;
+        // if == ? else == :
+        int a = 6;
         int b = 5;
+        /*
         if (a==b)
             resultado = "verdadeiro";
         else
             resultado = "Falso";
-        
+        */
+        String resultado = a==b ? "verdadeiro" : "Falso" ;
+
 
         System.out.println(resultado);
     }
