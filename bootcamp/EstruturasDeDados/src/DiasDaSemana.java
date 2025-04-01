dda8b22d60ccaee47ab6a56a67ad16f69b6c8f7b
pedro <pedrosilvanovais7@gmail.com>
2025-04-01 17:21:55 -0300

    exercicio Switch Case

---
+++ bootcamp/EstruturasDeDados/src/DiasDaSemana.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DiasDaSemana {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero para ver o dia equivalente: ");
+        int dia = sc.nextInt();
+        switch (dia) {
+            case 1:
+            System.out.println("Domingo");
+                break;
+            case 2:
+            System.out.println("Segunda-Feira");
+            System.out.println("É dia util !");
+                break;
+            case 3:
+            System.out.println("Terça-Feira");
+            System.out.println("É dia util !");
+                break;
+            default:
+                break;
+        }
+
+
+    }
+}
--- bootcamp/EstruturasDeDados/src/Medidas.java
+++ bootcamp/EstruturasDeDados/src/Medidas.java
@@ -1,22 +1,23 @@
 public class Medidas {
     public static void main(String[] args) {
-        String sigla = "M";
+        String sigla = "P";
         switch (sigla) {
-            case "P":
+            case "P":{
             System.out.println("Tamanho Pequeno !");
                 break;
-
-            case "M": 
+            }
+            case "M":{ 
             System.out.println("Tamanho Medio !");
-                break;
-
-            case "G":
+               break;
+            }
+            case "G":{
             System.out.println("Tamanho Grande !");
                 break;
-
-            default:
+            }
+            default:{
             System.out.println("Tamanho inexistente !");
                 break;
+           }
         }
     }
 }
