a2430ea0c5ddda25d24329bf611624f081d5a67f
pedro <pedrosilvanovais7@gmail.com>
2025-04-03 20:42:17 -0300

    usando Do While

---
+++ bootcamp/EstruturasDeRepetição/src/DoWhile.java
@@ -0,0 +1,12 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 0;
+        do{
+            System.out.println("Hello, World");
+            n++;
+        }
+        while(n < 5);
+        // garante fazer pelo menos uma vez 
+    }
+
+}
