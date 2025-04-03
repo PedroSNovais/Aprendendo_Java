79bbd64690225789a8bf2658c76243e4903b21d1
pedro <pedrosilvanovais7@gmail.com>
2025-04-03 17:09:30 -0300

    fiz DiasDaSemana.java

--- bootcamp/EstruturasDeDados/src/DiasDaSemana.java
+++ bootcamp/EstruturasDeDados/src/DiasDaSemana.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class DiasDaSemana {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um numero para ver o dia equivalente: ");
+        System.out.print("Digite um numero para ver o dia equivalente: ");
         int dia = sc.nextInt();
-        switch (dia) {
+        switch(dia){
             case 1:
             System.out.println("Domingo");
                 break;
@@ -17,7 +17,23 @@ public class DiasDaSemana {
             System.out.println("Terça-Feira");
             System.out.println("É dia util !");
                 break;
+            case 4:
+            System.out.println("Quarta-Feira");
+            System.out.println("É dia util !");
+                break;
+            case 5: 
+            System.out.println("Quinta-feira");
+            System.out.println("É util !!");
+                break;
+            case 6: 
+            System.out.println("Sexta-feira");
+            System.out.println("É dia util");
+                break;
+            case 7:
+            System.out.println("Sábado");
+                break;
             default:
+            System.out.println("digitou um numero invalido");
                 break;
         }
 //https://www.respondeai.com.br/conteudo/programacao/c/lista-de-exercicios/comando-switch-2025
