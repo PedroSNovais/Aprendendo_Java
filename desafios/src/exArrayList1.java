2a3256d3313887f3aca4db8ddf4977254b13d218
pedro <pedrosilvanovais7@gmail.com>
2025-04-08 16:06:47 -0300

    exercicio 1 de Arraylist

--- desafios/src/exArrayList1.java
+++ desafios/src/exArrayList1.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class exArrayList1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<String> compras = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int resp = 1;
+        String item = "";
+       
+            while (resp == 1){
+                System.out.println("--------------------------------------");
+                System.out.print("Digite 1 caso queira continuar: ");
+                resp = sc.nextInt();
+                    if(resp != 1)
+                        break;
+                    else{
+                        System.out.print("Digite um item para adicionar na lista: ");
+                        item = sc.next(); 
+                        compras.add(item);
+                    }
+            } 
+        System.out.println("--------------------------------------");
+        System.out.println("A lista de compras:");
+        System.out.println(compras);
+        System.out.println("<<<< Programa Finalizado >>>>>");
     }
 }
