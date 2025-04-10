c04755895a1dd55c67cccb00352793f1f5c32838
pedro <pedrosillvanovais7@gmail.com>
2025-04-10 13:46:34 -0300

    exercicio final final terminado

--- bootcamp/DesafioFinalSintaxeBasica/src/ContaFinal.java
+++ bootcamp/DesafioFinalSintaxeBasica/src/ContaFinal.java
@@ -6,20 +6,14 @@ public class ContaFinal {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Digite o saldo da conta: ");
         double saldoInicial = teclado.nextDouble();
-        System.out.print("Digite o valor da operação que deseja fazer: [valor negativo significa saque!] ");
-        double valor = teclado.nextDouble();
-        if(valor < 0){
-            double saldoFinal = saldoInicial + valor;
-            System.out.println(saldoFinal);
+        double saldoFinal = saldoInicial;
+
+        for(int n= 0; n < 3 ; n++) {
+            System.out.print("Digite o valor da operação: [valor negativo significa saque!] ");
+            double valor = teclado.nextDouble();
+            saldoFinal = saldoFinal + valor;
         }
-    }
-
-
-    public double sacar(double saldoInicial, double valorSaque)
-    {
-        double saldoFinal = saldoInicial - valorSaque;
-        return saldoFinal;
-
+        System.out.printf("O saldo final da conta é %.2f \n", saldoFinal);
     }
 }
 
