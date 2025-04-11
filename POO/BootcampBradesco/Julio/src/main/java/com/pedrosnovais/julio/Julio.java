3b2ac5c56a48037ec2002ee0a65350ed24ef44c4
pedro <pedrosilvanovais7@gmail.com>
2025-04-11 15:32:44 -0300

    trabalhando com netBeans e versionando com vscode

---
+++ POO/BootcampBradesco/Julio/src/main/java/com/pedrosnovais/julio/Julio.java
@@ -0,0 +1,9 @@
+package com.pedrosnovais.julio;
+public class Julio {
+    public static void main(String[] args) {
+        System.out.println("Hello Julio!");
+        /* 
+        * Julio do Guaiu me ensinou a usar o netbeans
+        */
+    }
+}
