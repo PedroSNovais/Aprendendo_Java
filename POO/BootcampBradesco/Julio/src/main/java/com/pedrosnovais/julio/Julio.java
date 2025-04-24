ca0e6008514d031342439a564139c7256eebf4c2
PedroSNovais <pedrosilvanovais7@gmail.com>
2025-04-24 16:44:07 -0300

    Organizando repositorio

--- POO/BootcampBradesco/Julio/src/main/java/com/pedrosnovais/julio/Julio.java
+++
@@ -1,9 +0,0 @@
-package com.pedrosnovais.julio;
-public class Julio {
-    public static void main(String[] args) {
-        System.out.println("Hello Julio!");
-        /* 
-        * Julio do Guaiu me ensinou a usar o netbeans
-        */
-    }
-}
